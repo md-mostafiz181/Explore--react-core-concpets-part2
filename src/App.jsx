f8e66b8e108207d979246fb321a2c57a1cd90fbd
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-10-16 09:46:35 +0600

    added different kind of handle clicked

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,26 @@ import './App.css'
 function App() {
 
 
+  function button1 (){
+    alert("button1 is clicked")
+  };
+
+  const button2 = ()=>{
+    alert("button2 is clicked")
+  }
+  
+
+ const button4 =(num)=>{
+  alert(num + 5)
+ }
   return (
     <>
 
       <h1>Vite + React</h1>
+      <button onClick={button1}>Button 1</button>
+      <button onClick={button2}>Button 2</button>
+      <button onClick={ ()=> alert("button3 is clicked")}>Button 3</button>
+      <button onClick={()=>button4(5)}>Button 4</button>
       
     </>
   )
